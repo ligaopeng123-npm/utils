dcd11b3c2621fea3b86f38be87caee76b2883c78
ligaopeng123 <gaopeng@123>
2022-02-17 08:32:56 +0800

    合入问题：去掉sourcemap 合入人：pgli 合入时间：2022年2月17日08:32:52

---
+++ packages/date/tsdx.config.js
@@ -0,0 +1,6 @@
+// @ts-ignore
+const tsdxConfig = require("../../tsdx.config");
+
+module.exports = {
+    rollup: tsdxConfig,
+}
---
+++ tsdx.config.js
@@ -0,0 +1,6 @@
+// rollup配置
+module.exports = function (config, options) {
+    // 去掉sourcemap
+    config.output.sourcemap = false;
+    return config
+}
