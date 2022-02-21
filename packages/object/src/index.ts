f22c16b0ef3766cb840192a2401992482075ede3
ligaopeng123 <gaopeng@123>
2022-02-21 09:25:50 +0800

    合入问题：显式导入再导出 合入人：pgli 合入时间：2022年2月21日09:21:22

--- packages/object/src/index.ts
+++ packages/object/src/index.ts
@@ -1,40 +1,31 @@
+import {clone, cloneAllItems, cloneAllArray, cloneAllObject} from './clone';
+import {mousePosition} from './mouse';
+import {getStyle, parentByExpected, copyText} from './dom';
+import {default as assignDeep, assignDeepMergeArray, assignDeepNotIncludedArray} from "./assignDeep";
+import {filterObject, mapObject, forEachObject} from './objectLoop';
+import {default as length} from "./length";
 /**
  * clone函数
  */
-export {clone, cloneAllItems, cloneAllArray, cloneAllObject} from './clone';
+export {clone, cloneAllItems, cloneAllArray, cloneAllObject};
 /**
  * 鼠标位置
  */
-export {mousePosition} from './mouse';
+export {mousePosition}
 /**
- * 获取样式
+ * 1、获取样式 2、获取符合预期的父节点 2、文本复制
  */
-export {getStyle} from './dom';
-/**
- * 获取符合预期的父节点
- */
-export {parentByExpected} from './dom';
-/**
- * 文本复制
- */
-export {copyText} from './dom';
+export {getStyle, parentByExpected, copyText}
+
 /**
  * 获取数组 对象长度
  */
-export {default as length} from "./length";
+export {length}
 /**
  * 对象属性合并 数组默认覆盖  对象属性合并（数组是concat合并） 对象属性合并（数组不做处理）
  */
-export {default as assignDeep, assignDeepMergeArray, assignDeepNotIncludedArray} from "./assignDeep";
+export {assignDeep, assignDeepMergeArray, assignDeepNotIncludedArray};
 /**
- * 对象的过滤循环
+ * 对象的过滤循环  对象的map循环  对象的forEach循环
  */
-export {filterObject} from './objectLoop';
-/**
- * 对象的map循环
- */
-export {mapObject} from './objectLoop';
-/**
- *
- */
-export {forEachObject} from './objectLoop';
+export {filterObject, mapObject, forEachObject};
