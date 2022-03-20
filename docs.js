6999988f2fd3f2e12e3e889137bbf458028e5dcc
ligaopeng123 <1170234932@qq.com>
2022-03-20 14:38:50 +0800

    文档更新

--- docs.js
+++ docs.js
@@ -5,7 +5,7 @@ const {getMDFile} = require("./bin/http");
  * 文件copy 将md文件爬出来 放到docs中 提高访问速度 网速好的话 可以全量处理
  */
 copyFiles(path.resolve(__dirname, 'document'), path.resolve(__dirname, 'docs')).then(() => {
-    job();
+   job();
 });
 
 const job = () => {
