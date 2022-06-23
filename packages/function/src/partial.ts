e7a3399ed39c1b281e3ac2b8e1f064cab09b59e4
ligaopeng123 <gaopeng@123>
2022-06-23 09:03:36 +0800

    合入问题：partial函数参数缓存修改 合入人：pgli 合入时间：2022年6月23日09:03:23

--- packages/function/src/partial.ts
+++ packages/function/src/partial.ts
@@ -11,14 +11,16 @@
  **********************************************************************/
 type PartialFn = (...args: Array<any>) => any;
 const partial = (fn: PartialFn, ...args: Array<any>): any => {
-	return (...args2: Array<any>) => {
-		let current = 0;
-		for (let i = 0; i < args.length && current < args2.length; i++) {
-			// 如果参数中有undefined 则使用后面传入的参数做替换
-			if (args[i] === undefined) args[i] = args2[current++];
-		}
-		return fn(...args);
-	}
+    return (...args2: Array<any>) => {
+        let current = 0;
+        // 定义当前参数，避免执行后被污染
+        const curArgs = [...args];
+        for (let i = 0; i < args.length && current < args2.length; i++) {
+            // 如果参数中有undefined 则使用后面传入的参数做替换
+            if (args[i] === undefined) curArgs[i] = args2[current++];
+        }
+        return fn(...curArgs);
+    }
 };
 
 
