6a2d22fbeae66819061ee668112ee65ccc17b488
ligaopeng123 <1170234932@qq.com>
2024-08-28 11:42:47 +0800

    feat: add FunctionCallbackReturnType

--- packages/tsTypes/src/index.ts
+++ packages/tsTypes/src/index.ts
@@ -1,5 +1,7 @@
 // 定义函数callback的类型及返回值
 export type FunctionCallback<T extends any[], R> = (...arg: T) => R;
+// T是函数则返回函数返回值 否则返回T
+export type FunctionCallbackReturnType<T> = T extends (...args: any[]) => infer R ? R : T;
 // Optional<params, 'name' | ‘age’>； 将params的 name age 制成可选项
 export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 // 获取一个对象的key和value的类型
